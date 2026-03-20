b3da3bf3fcefc8715ae22e42326947c69f99b46a
Juan López Rubio <j.lopez.r@gmail.com>
2026-03-20 20:24:14 +0100

    Ejemplo y TO-DO

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- [ ] El yaw parece que esta girado 90 grados. En la imagen, la carretera (no las autopistas) son el meridiano de Greenwich que van en direccion 0 grados.
+
+- [ ] Si cambio el roll parece que esta cambiando realmente el yaw.
--- example.bat
+++ example.bat
@@ -1,11 +1,181 @@
 python nadir_cam.py ^
   --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
   --layer "OI.OrthoimageCoverage" ^
-  --lat 41.276361469991926 ^
-  --lon 1.988489175067195 ^
-  --alt 240 ^
-  --yaw 30 ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 0 ^
   --hfov 70 ^
   --width 1280 ^
   --height 720 ^
-  --out frame.png
+  --out yaw00.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 45 ^
+  --roll 0 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out yaw45.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 90 ^
+  --roll 0 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out yaw90.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out pitch00.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 10 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out pitch10.png
+
+  python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 20 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out pitch20.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 30 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out pitch30.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 45 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out pitch45.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 0 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out roll00.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 10 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out roll10.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 20 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out roll20.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 30 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out roll30.png
+
+python nadir_cam.py ^
+  --wms-url "https://www.ign.es/wms-inspire/pnoa-ma" ^
+  --layer "OI.OrthoimageCoverage" ^
+  --lat 41.525242092699614 ^
+  --lon 0.061412513999926206 ^
+  --alt 1000 ^
+  --yaw 0 ^
+  --roll 45 ^
+  --pitch 0 ^
+  --hfov 70 ^
+  --width 1280 ^
+  --height 720 ^
+  --out roll45.png
